ca7ab88a14428bd21209256b72dfbbc42f2b26a5
abdelilah <abdelilahghoummach21@gmail.com>
2024-11-02 23:14:30 +0100

    functions

---
+++ 4_functions/script.js
@@ -0,0 +1,12 @@
+// function declaration
+function greet(name) {
+  return `Hello,${name}`;
+}
+
+// function expression
+const greet = function (name) {
+  return `Hello, ${name}`;
+};
+
+//arrow function
+const greet = (name) => `Hello, ${name}`;
