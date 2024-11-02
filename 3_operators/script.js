d8c5963a43aa7a9a5cfed60643c455ebcc6484ba
abdelilah <abdelilahghoummach21@gmail.com>
2024-11-02 23:10:32 +0100

    operators

---
+++ 3_operators/script.js
@@ -0,0 +1,17 @@
+// Arithmetic
+let sum = 10 + 5;
+let diff = 10 - 5;
+let prod = 10 * 5;
+let quotient = 10 / 5;
+let remeinder = 10 % 5;
+
+// Comparison
+let isEqual = 10 == "10"; // true
+let isStrictEqual = 10 === "10"; // false
+let isNotEqual = 10 !== 5; //tue
+let isStrictNotEqual = 10 !== 5; //true
+
+//Logical
+let and = true && false; //false
+let or = true || false; //true
+let not = !true; // false
