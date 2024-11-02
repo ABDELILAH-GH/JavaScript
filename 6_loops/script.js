1145aa5021c862cb8d5d7ec224a054155e78bbfa
abdelilah <abdelilahghoummach21@gmail.com>
2024-11-02 23:21:50 +0100

    loops

---
+++ 6_loops/script.js
@@ -0,0 +1,18 @@
+// for loop
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+//while loop
+let i = 0;
+while (i < 10) {
+  console.log(i);
+  i++;
+}
+
+//Do-while loop^
+let j = 0;
+do {
+  console.log(j);
+  j++;
+} while (j < 10);
