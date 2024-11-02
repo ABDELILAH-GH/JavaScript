53918cea2752a1fe73000a31a5973ab48c0e26aa
abdelilah <abdelilahghoummach21@gmail.com>
2024-11-02 23:29:20 +0100

    dom manipulation selecting elements

---
+++ 7_dom/script.js
@@ -0,0 +1,8 @@
+// dom manupulation selecting elements
+let element = document.getElementById("id");
+
+let element1 = document.getElementsByClassName("class");
+
+let element3 = document.querySelector("selector");
+
+let element4 = document.querySelectorAll("selector");
