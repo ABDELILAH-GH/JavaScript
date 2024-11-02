187f052c47f82f792c466946afaf457fcb0eab53
abdelilah <abdelilahghoummach21@gmail.com>
2024-11-02 23:02:22 +0100

    Data Types

---
+++ 2_Data-types/script.js
@@ -0,0 +1,11 @@
+// premitive
+let str = "hello";
+let num = 1000;
+let bool = true;
+let undef; // undefined
+let n = null;
+let sym = Symbol("symbol");
+
+// Non-primitive
+let arr = [1, 2, 3];
+let obj = { name: "hassan", age: 30 };
